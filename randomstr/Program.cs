ecaf610500ceec5418465eaaac3804c2bbf18c61
shimarisu <pe4jun@live.jp>
2018-01-25 17:28:46 +0900

    新規に作成

--- randomstr/Program.cs
+++ randomstr/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace randomstr
 {
@@ -10,6 +6,19 @@ namespace randomstr
     {
         static void Main(string[] args)
         {
+            String chars = "ABCDEFGHIJKLIMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/!$%&'()=~|@`[]{}*+<>?_;:,.\\";
+            char[] charArray = chars.ToCharArray();
+            String randomStr = "";
+            int size = 10;
+            Random cRondom = new System.Random();
+//            int i;
+            for (int i = 0; i < size; i++)
+            {
+                int len = chars.Length;
+                int rndNum = cRondom.Next(len);
+                randomStr = randomStr + charArray[rndNum];
+            }
+            Console.WriteLine(randomStr);
         }
     }
 }
