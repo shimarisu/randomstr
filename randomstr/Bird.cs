2da8683393645b0054a62e32be608a06bbccc63a
shimarisu <pe4jun@live.jp>
2018-02-07 10:22:42 +0900

    add Bird.cs

---
+++ randomstr/Bird.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomstr
+{
+    abstract class Bird
+    {
+        //  名前フィールド
+        private String name;
+        //  引数つきコンストラクタ
+        public Bird(String name)
+        {
+            this.name = name;
+        }
+        // 名前を取得
+        public String Name
+        {
+            get { return name; }
+        }
+        //  鳴く（抽象メソッド）
+        public abstract void Sing();
+
+    }
+}
