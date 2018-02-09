b9e54d9cda03a87ca845853e100f02f91eab1fed
shimarisu <pe4jun@live.jp>
2018-02-09 15:28:10 +0900

    カラスの追加

---
+++ randomstr/Crow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomstr
+{
+    class Crow : Bird
+    {
+        public Crow() : base("カラス")
+        {
+        }
+        //  カラスがなく
+        public override void Sing()
+        {
+            Console.WriteLine("カーカー");
+        }
+
+    }
+}
